7bbde629217d90ec3f1bf788c41e7df163cac0fb
Tharinda <trajapaksha@ariasystems.com>
2024-11-28 16:07:29 +0530

    Modify navigation bar styles

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -3,14 +3,16 @@ import {TABS} from './../config/config'
 
 const Sidebar = ({setTab}) => {
   return (
-      <div className="fixed top-0 left-0 w-full bg-gray-800 text-white flex justify-evenly items-center h-16">
-        {
-          TABS.map((tab) => (
-              <button key={tab} onClick={() => setTab(tab)} className="p-4 text-left hover:bg-gray-600">
-                {tab}
-              </button>
-          ))
-        }
+      <div className="fixed top-0 left-0 w-full bg-gray-800 text-white flex justify-center items-center h-16">
+        <div className="flex space-x-4">
+          {
+            TABS.map((tab) => (
+                <button key={tab} onClick={() => setTab(tab)} className="px-4 py-2 bg-gray-700 hover:bg-gray-600 rounded text-sm font-medium">
+                  {tab}
+                </button>
+            ))
+          }
+        </div>
       </div>
   )
 }
