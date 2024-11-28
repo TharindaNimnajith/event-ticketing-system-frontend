7dd0aa88d9855617cab16f8e8935168c9ebafeb8
Tharinda <trajapaksha@ariasystems.com>
2024-11-28 16:11:05 +0530

    Modify ticketing configurations styles

--- src/page/Configurations.jsx
+++ src/page/Configurations.jsx
@@ -23,7 +23,7 @@ const Configurations = () => {
   }, [])
 
   return (
-      <div>
+      <div className="flex flex-col items-center">
         <h2 className="text-xl font-bold mb-4">Ticketing Configurations</h2>
         {
           loading ? (
@@ -31,12 +31,32 @@ const Configurations = () => {
           ) : error ? (
               <ErrorAlert error={error}/>
           ) : (
-              <div>
-                <p><strong>Max Ticket Capacity:</strong> {data?.data?.max_ticket_capacity}</p>
-                <p><strong>Total Tickets:</strong> {data?.data?.total_tickets}</p>
-                <p><strong>Ticket Release Rate (In Seconds):</strong> {data?.data?.ticket_release_rate}</p>
-                <p><strong>Customer Retrieval Rate (In Seconds):</strong> {data?.data?.customer_retrieval_rate}</p>
-              </div>
+              <table className="table-auto border-collapse border border-gray-400">
+                <thead>
+                  <tr>
+                    <th className="border border-gray-400 px-4 py-2">Configuration</th>
+                    <th className="border border-gray-400 px-4 py-2">Value</th>
+                  </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <td className="border border-gray-400 px-4 py-2">Max Ticket Capacity</td>
+                  <td className="border border-gray-400 px-4 py-2">{data?.data?.max_ticket_capacity}</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-400 px-4 py-2">Total Tickets</td>
+                  <td className="border border-gray-400 px-4 py-2">{data?.data?.total_tickets}</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-400 px-4 py-2">Ticket Release Rate (In Seconds)</td>
+                  <td className="border border-gray-400 px-4 py-2">{data?.data?.ticket_release_rate}</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-400 px-4 py-2">Customer Retrieval Rate (In Seconds)</td>
+                  <td className="border border-gray-400 px-4 py-2">{data?.data?.customer_retrieval_rate}</td>
+                </tr>
+                </tbody>
+              </table>
           )
         }
       </div>
